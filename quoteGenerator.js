fd1becb18090e6bf140161593dbaffd28a64867e
Prisca Roseline <prisca.ros@onja.org>
2020-06-05 10:37:01 +0300

    I created quotes generator random

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,33 +1,29 @@
  console.log('it works!');
 
 // I creat an Array which is inside of object in order to store these strings.
-var quotesGenerator = {
-            quote1: [ "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.", "Start by doing what's necessary", "Francis of Assisi"],
 
-	       quote2: ["Believe you can and you're halfway there.","Believe you can and you're halfway there.","Theodore Roosevelt"],
+           let quote1 = [ "Start by doing what's necessary","then what's possible","and suddenly you are doing the impossible."]
+
+	       let quote2 = ["Believe you can and you're halfway there.","Believe you can and you're halfway there.","Theodore Roosevelt"]
 	    
-            quote3: ["It does not matter how slowly you go as long as you do not stop.","Believe you can and you're halfway there.","Confucius"],
-
-            quote4: ["Our greatest weakness lies in giving up.","The most certain way to succeed is always to try just one more time.","Thomas A. Edison"]
-    };
-
-    console.log(quotesGenerator);
+           let quote3 = ["It does not matter how slowly you go as long as you do not stop.","Believe you can and you're halfway there", "Theodore Roosevelt"]
+    
 
     // I creat a function in order to conctenate the strings
 
-    function newQuotesGen(quotes) {
-        var quotesGenArray = [];
-        for (let i = 0; i < quotes.length; i++) {
-            if(quotes.length >= quotes[3]) {
-               quotesGenArray.push(`${quotes[i]}`);
-        };
-    };
-} 
+    function newQuotesGen() {
+        let randomNumber1 = Math.floor((Math.random() * (quote1.length)));
+        let newRandomNumber1 = quote1[randomNumber1];
+    
+        let randomNumber2 = Math.floor((Math.random() * (quote2.length)));
+        let newRandomNumber2 = quote2[randomNumber2];
+    
 
-    // I creat a prompt in order interact with a user
+        let randomNumber3 = Math.floor((Math.random() * (quote3.length)));
+        let newRandomNumber3 = quote3[randomNumber3];
 
-  let randomQoutes = prompt("Set your number of qoutes here");
-  let randomQuotes = "";
-  if (randomQuotes !== null){
-    alert(`${randomQuotes}`)
-};    
+    //  console the code
+
+    console.log(`${newRandomNumber1}, ${newRandomNumber2}, ${newRandomNumber3}`);
+    };newQuotesGen();
+    
