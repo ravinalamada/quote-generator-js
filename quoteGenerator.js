efab84bb44ac5d70e0d2cd32af9c914cca790021
Prisca Roseline <prisca.ros@onja.org>
2020-06-05 12:02:53 +0300

    I created a code that choose quotes generated (from 1 to 5)

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -11,7 +11,7 @@
 
     // I creat a function in order to conctenate the strings
 
-    function newQuotesGen() {
+    function quotesGen() {
         let randomNumber1 = Math.floor((Math.random() * (quote1.length)));
         let newRandomNumber1 = quote1[randomNumber1];
     
@@ -23,7 +23,37 @@
         let newRandomNumber3 = quote3[randomNumber3];
 
     //  console the code
-
     console.log(`${newRandomNumber1}, ${newRandomNumber2}, ${newRandomNumber3}`);
-    };newQuotesGen();
+    };quotesGen();
     
+   // I creat prompt in order to interact with a user and choose number of quotes generated (from 1 to 5)
+   function newQuotesGen() { 
+      let quotesNumber = prompt("Choose your number of quotes here,'1' ");
+
+   if (quotesNumber !== null) {
+        var randomNumber1 = Math.floor(Math.random() *  (quote1.length));
+        var randomNumber2 = Math.floor(Math.random() *  (quote2.length));
+        var randomNumber3 = Math.floor(Math.random() *  (quote3.length));
+        var randomQuote = quote1[randomNumber1] + ` ` + quote2[randomNumber2] + ` ` + quote3[randomNumber3]
+        for (let i = 0; i < 5; i++) {
+            console.log(randomQuote) 
+        };
+    };
+}newQuotesGen()
+    // Choose 2 types of quotation generated 
+
+    let newQuote1 = ["Let's have a break", "I have been waiting for that for a long time"];
+    let newQuote2 = ['Bravo, you got this', 'Yep, as you see'];
+
+    // Let's have another function
+
+    function optionQuoteGen() {
+        let quotesNumber = prompt("Choose your number of quotes here,'1' ");
+        if (quotesNumber !== null) {
+            var otherRandomNumber1 = Math.floor((Math.random() * (newQuote1.length)));
+            var otherRandomNumber2 = Math.floor((Math.random() * (newQuote2.length)));
+            var otherRandomQutes = newQuote1[otherRandomNumber1] + ' ' + newQuote2[otherRandomNumber2];
+            console.log(otherRandomQutes);
+        };    
+    }optionQuoteGen();
+
