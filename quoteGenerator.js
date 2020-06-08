eb300ee2b71e2bf97f335cc4403f0c6c146936f8
Prisca Roseline <prisca.ros@onja.org>
2020-06-08 11:41:48 +0300

    Generated an other propmt and arrays

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -26,15 +26,15 @@
     console.log(`${newRandomNumber1}, ${newRandomNumber2}, ${newRandomNumber3}`);
     };quotesGen();
 
-    // Generate a function
+    /* Creat a function
+      function genQuotesNum() {
 
-    function genQuotesNum() {
-        
     //1. I creat prompt in order to interact with a user and choose number of quotes generated (from 1 to 5)
-        let quotesNumber = prompt("Choose your number of quotes here,'1' ");
-                
+    let quotesNumber = prompt("Choose your number of quotes here,'1' ");
+
     //2. check if the prompt give us something between 1 and 5
             if (quotesNumber > 1 && quotesNumber < 5) {
+
     //3. Generate a loop in order to last the number of times that the user entered in the prompt.            
                 for (let i = 0; i < quotesNumber; i++) {
                     let randomIndex1 = Math.floor((Math.random() * (quote1.length)));
@@ -49,25 +49,39 @@
             return "Not a valid number"
         };
 }genQuotesNum();
-    // Choose 2 types of quotation generated
-    
-//3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!
-//4. refactor our code into a function, and put the function call inside the loop
-//4.1 generate random numbers
-//4.2 show the generated quotes 
 
-    /*let newQuote1 = ["Let's have a break", "I have been waiting for that for a long time"];
+*/
+
+/* Choose between 2 types of quotation generators (you should therefore have 2 sets of sentences)
+
+Once the quotes have been generated, offer to generate new quotes or stop the program there
+
+Adapt the program so that it can offer these features. It's your turn !*/
+
+// Second type of the exercise
+
+    let newQuote1 = ["Let's have a break", "I have been waiting for that for a long time"];
     let newQuote2 = ['Bravo, you got this', 'Yep, as you see'];
+    let newQuote3 = ["Let's have a break", "I have been waiting for that for a long time"]
 
-    // Let's have another function
+    // Let's have another function 
 
     function optionQuoteGen() {
+
+    // GEnerate a prompt in order to ask the user which type of quotation they want to use   
         let quotestypes = prompt("Choose your type of quotes here");
-        if (quotestypes !== null) {
-            var otherRandomNumber1 = Math.floor((Math.random() * (newQuote1.length)));
-            var otherRandomNumber2 = Math.floor((Math.random() * (newQuote2.length)));
-            var otherRandomQutes = newQuote1[otherRandomNumber1] + ' ' + newQuote2[otherRandomNumber2];
-            console.log(otherRandomQutes);
-        };    
-    }optionQuoteGen();*/
+
+            if (quotestypes < 2) {
+                let otherRandoIndex1 = Math.floor((Math.random() * (newQuote1.length)));
+                let newString1 = newQuote1[otherRandoIndex1];
+                console.log(`${newString1}`)
+            }else if(quotestypes >= 2) {
+                let otherRandomIndex2 = Math.floor((Math.random() * (newQuote2.length)));
+                let newString2 = newQuote2[otherRandomIndex2];
+                let otherRandomIndex3 = Math.floor((Math.random() * (newQuote3.length)));
+                let newString3 = newQuote3[otherRandomIndex3];
+                console.log(`${newString2}, ${newString3}`);
+            };  
+
+    }optionQuoteGen();
 
